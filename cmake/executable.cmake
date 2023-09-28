b8faba7ce18655abcd2923fc679330abac3d93be
qiaozhijian <935514540@qq.com>
2023-09-28 23:35:39 +0800

    update

---
+++ cmake/executable.cmake
@@ -0,0 +1,11 @@
+add_library(${PROJECT_NAME} SHARED
+        ${ALL_SRCS})
+target_link_libraries(${PROJECT_NAME}
+        ${ALL_TARGET_LIBRARIES})
+
+#add_subdirectory(backward-cpp)
+
+#add_executable(template_node examples/cpp/template_node.cpp ${BACKWARD_ENABLE})
+add_executable(template_node examples/cpp/template_node.cpp)
+#add_backward(template_node)
+target_link_libraries(template_node ${PROJECT_NAME})
